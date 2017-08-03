13c8336c2de4db1c55ee45d3382ed349d8940aae
Daniel <lanigela@gmail.com>
2017-08-03 16:23:54 -0400

    debug

--- app/code/community/Exocortex/Threekit/Model/Observer.php
+++ app/code/community/Exocortex/Threekit/Model/Observer.php
@@ -51,8 +51,7 @@ class Exocortex_Threekit_Model_Observer
 
             if(count($additionalOptions) > 0){
                 $item->addOption(array(
-                    'product_id' => $item->getProductId(),
-                    'code' => 'additional_options',
+                    'code' => 'additional_options'
                     'value' => serialize($additionalOptions)
                 ));
             }
