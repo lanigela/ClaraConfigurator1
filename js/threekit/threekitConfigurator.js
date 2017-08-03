c81e1670e946ff97af747804c98bc2f6c1c4d84f
Daniel <lanigela@gmail.com>
2017-08-03 16:13:36 -0400

    debug

--- js/threekit/threekitConfigurator.js
+++ js/threekit/threekitConfigurator.js
@@ -97,7 +97,11 @@
       var xhr = new XMLHttpRequest();
       xhr.open('POST', self.options.submitUrl, true);
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
+      xhr.onload = function() {
+        if (xhr.status == 302) {
+          document.location.href = xhr.responseURL;
+        }
+      };
       xhr.send(postParams);
       /*$.ajax({
         url: self.options.submitUrl,
